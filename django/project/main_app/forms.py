15db8a9f2058f176ac8d6a3164dacdc88e11e706
Roland Schiebel <rschiebel@vmware.com>
2019-03-21 12:04:50 -0400

    remove duplicate django folder

--- django/project/main_app/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-
-from .models import CrawlRequest
-
-class CrawlRequestForm(ModelForm):
-    class Meta:
-        model = CrawlRequest
-        exclude = ('user', 'created', 'modified', 'docs_collected', 'status')
--- django/project/webcrawler/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render, redirect
-
-def welcome(request):
-    if request.user.is_authenticated:
-        return redirect('mainapp_home')
-    else:
-        return render(request, 'webcrawler/welcome.html')
