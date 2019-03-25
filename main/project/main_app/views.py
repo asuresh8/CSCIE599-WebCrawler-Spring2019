41f421453a98761908bb3455367b64461f77fd82
Ansuman Prusty <ansumanprusty@gmail.com>
2019-03-24 23:56:26 -0400

    Authentication requirements and token generation.

--- main/project/main_app/views.py
+++ main/project/main_app/views.py
@@ -4,6 +4,16 @@ from django.contrib.auth.decorators import login_required
 from .models import CrawlRequest
 from .forms import CrawlRequestForm
 
+from rest_framework.response import Response
+from rest_framework.permissions import BasePermission, IsAuthenticated, AllowAny
+from rest_framework.views import APIView
+from rest_framework.generics import RetrieveUpdateAPIView
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework_jwt.utils import jwt_payload_handler
+import jwt
+from django.conf import settings
+from django.contrib.auth.signals import user_logged_in
 
 @login_required()
 def home(request):
@@ -13,6 +23,38 @@ def home(request):
     jobs = CrawlRequest.objects.filter(user=user)
     return render(request, "main_app/home.html", {'form': form, 'jobs': jobs})
 
+"""
+@api_view(['POST'])
+@permission_classes([AllowAny, ])
+def authenticate_user(request):
+
+    try:
+        email = request.data['email']
+        password = request.data['password']
+
+        user = User.objects.get(email=email, password=password)
+        if user:
+            try:
+                payload = jwt_payload_handler(user)
+                token = jwt.encode(payload, settings.SECRET_KEY)
+                user_details = {}
+                user_details['name'] = "%s %s" % (
+                    user.first_name, user.last_name)
+                user_details['token'] = token
+                user_logged_in.send(sender=user.__class__,
+                                    request=request, user=user)
+                return Response(user_details, status=status.HTTP_200_OK)
+
+            except Exception as e:
+                raise e
+        else:
+            res = {
+                'error': 'can not authenticate with the given credentials or the account has been deactivated'}
+            return Response(res, status=status.HTTP_403_FORBIDDEN)
+    except KeyError:
+        res = {'error': 'please provide a email and a password'}
+    return Response(res)
+"""
 
 @login_required()
 def new_job(request):
