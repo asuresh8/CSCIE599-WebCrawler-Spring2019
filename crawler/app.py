f9406c7ea6a73fd2a9a70c603fcc02b87ae0f7eb
Oscar Mejia <oscmejia@gmail.com>
2019-04-01 16:51:35 -0500

    test K API

--- crawler/app.py
+++ crawler/app.py
@@ -3,54 +3,69 @@ from flask import request
 from redis_connect import testConnectionRedis
 from crawler import crawlUrl
 import os, _thread, sys, time
+import requests
 
 app = Flask(__name__)
 urls = os.environ.get('URLS', '')
+releaseDate = os.environ.get('DATE', '0')
 
 @app.route('/')
 def main():
-    hname =  request.host_url
-    try:
-       requests.post('http://localhost:8002' + '/api/v1.0/register/crawlerID', json ={'name': hname})
-       #requests.post(os.environ.get('manager') + '/api/v1.0/register/crawlerID', json ={'name': hname})
-       return "crawler"
-    except  requests.RequestException as rex:
-       return "request exception"
-    except requests.HTTPException as hex:
-       return "http exception"
-    except Exception as ex:
-       return "general exception"
+  hname =  request.host_url
+  try:
+    requests.post('http://localhost:8002' + '/api/v1.0/register/crawlerID', json ={'name': hname})
+    #requests.post(os.environ.get('manager') + '/api/v1.0/register/crawlerID', json ={'name': hname})
+    return "crawler"
+  except  requests.RequestException as rex:
+    return "request exception"
+  except requests.HTTPException as hex:
+    return "http exception"
+  except Exception as ex:
+    return "general exception"
 
 @app.route('/crawl/')
 def do_crawl():
-    curUrl =  request.args.get('crawlurl')
-    print("start crawling")
-    s3uri , links =  crawlUrl(curUrl)
-    if links is None:
-	json_data = { 'main_url' : curUrl, 's3uri': "" ,  'child_urls': []}
-    else:
-        json_data = { 'main_url' : curUrl, 's3uri': s3uri ,  'child_urls':  json.dumps(links)}
-    try :
-       r = requests.post('http://localhost:8002' + '/api/v1.0/links', json=  json_data)
-       #r = requests.post(os.environ.get("manager") + '/api/v1.0/links', json=  json_data)
-       return  "ok"
-    except  requests.RequestException as rex:
-       return "request exception"
-    except requests.HTTPException as hex:
-       return "http exception"
-    except Exception as ex:
-       return "general exception"
+  curUrl =  request.args.get('crawlurl')
+  print("start crawling")
+  s3uri , links =  crawlUrl(curUrl)
+  if links is None:
+    json_data = { 'main_url' : curUrl, 's3uri': "" ,  'child_urls': []}
+  else:
+    json_data = { 'main_url' : curUrl, 's3uri': s3uri ,  'child_urls':  json.dumps(links)}
 
+  try :
+    r = requests.post('http://localhost:8002' + '/api/v1.0/links', json=  json_data)
+    #r = requests.post(os.environ.get("manager") + '/api/v1.0/links', json=  json_data)
+    return  "ok"
+  except  requests.RequestException as rex:
+    return "request exception"
+  except requests.HTTPException as hex:
+    return "http exception"
+  except Exception as ex:
+    return "general exception"
 
 def testConnections():
-    print(testConnectionRedis())
+  print(testConnectionRedis())
 
 def flaskThread():
   app.run(debug=False, host="0.0.0.0", port=8003, use_reloader=False)
+
+def managerPing():
+  managerUrl = f"http://crawler-manager-service-{releaseDate}.default/"
+  try:
+    requests.get(managerUrl)
+    return False
+  except  requests.RequestException as rex:
+    return "request exception"
+  except requests.HTTPException as htex:
+    return "http exception"
+  except Exception as ex:
+    return "general exception"
 
 if __name__ == "__main__":
     testConnections()
     _thread.start_new_thread(flaskThread,())
     print('Will kill crawler server after 15s -- urls', urls,file=sys.stderr)
+    managerPing()
     time.sleep(15)
     sys.exit(0)
--- crawler/crawler.py
+++ crawler/crawler.py
@@ -11,11 +11,11 @@ import redis_connect
 
 def crawlUrl(url):
     links = []
-    # connect to redis 
+    # connect to redis
     #r = redis.StrictRedis(host=config.REDIS_HOST, port= config.REDIS_PORT)
     try:
        if is_crawled(url) == True :
-          return  (None, None) 
+          return  (None, None)
        else:
           #get web page
           response = requests.get(url)
@@ -31,20 +31,20 @@ def crawlUrl(url):
        return (None, None)
 
 # store page  to  s3
-def store_s3(res):  
+def store_s3(res):
     try:
-      s3_client = boto3.client('s3', aws_access_key_id= config.S3_KEY, 
+      s3_client = boto3.client('s3', aws_access_key_id= config.S3_KEY,
                                      aws_secret_access_key= config.S3_SECRET_KEY)
       key = 'crawl_pages/' + str(uuid.uuid4().hex[:6])  + '.html'
       fp = BytesIO(res.content)
       s3_client.upload_fileobj(fp, config.S3_BUCKET, key)
-      s3uri = s3_client.generate_presigned_url('get_object', 
-                                               Params = {'Bucket' : config.S3_BUCKET, 'Key': key})  
-      print ('s3uri' + s3uri)    
+      s3uri = s3_client.generate_presigned_url('get_object',
+                                               Params = {'Bucket' : config.S3_BUCKET, 'Key': key})
+      print ('s3uri' + s3uri)
     except Exception as e:
       print(e.__traceback__)
       return None
-    
+
     return s3uri
 
 # get_links - parses the url 'a' tags using beautiful soup
@@ -53,7 +53,7 @@ def get_links(r, links):
     for link in bsObj.find_all('a'):
         if 'href' in link.attrs:
            links.append(url_for(link.attrs['href'], _external = True))
-    return 
+    return
 
 # add url and s3 location to central redis cache
 def add_key_cache(url,s3uri):
@@ -67,16 +67,19 @@ def add_key_cache(url,s3uri):
 
 # is_crawled - checks redis for existing of key
 def is_crawled(url):
-    return redis_connect.redisConnect != None and redis_connect.redisConnect.exists(url) ? True: False
+  if redis_connect.redisConnect != None and redis_connect.redisConnect.exists(url):
+    return True
+  else:
+    return False
 
-""" 
+"""
 def write_file(url):
-    
-    fname = str(uuid.uuid4().hex[:6])  + '.html' 
+
+    fname = str(uuid.uuid4().hex[:6])  + '.html'
     with open(fname, 'wb') as fp:
         for chunk in r.iter_content(chunk_size=512):
             fp.write(chunk)
-    return fname  
+    return fname
 """
 
 
