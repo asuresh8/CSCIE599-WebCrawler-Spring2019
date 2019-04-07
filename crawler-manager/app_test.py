397e4a1a3641178cdc6a52e55dab1cb36851a095
Adi Suresh <suresh.aditya@gmail.com>
2019-04-07 16:04:21 -0700

    add some crawler manager tests

--- crawler-manager/app_test.py
+++ crawler-manager/app_test.py
@@ -8,15 +8,26 @@ class TestCrawlerManagerApp(unittest.TestCase):
     """
 
     def setUp(self):
-        pass
+        app.app.testing = True
+        self.client = app.app.test_client()
 
     def tearDown(self):
         pass
     
-    # TODO: delete this noop test and write actual unit tests
-    def test_noop(self):
-        self.assertEqual(0, 0)
+    def test_home(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, b'Crawler Manager')
  
+    def test_ping(self):
+         response = self.client.get('/ping')
+         self.assertEqual(response.status_code, 200)
+         self.assertEqual(response.data, b'Crawler Manager PING')
+    
+    def test_kill(self):
+        response = self.client.post('/kill')
+        self.assertEqual(response.status_code, 200)
+
  
 if __name__ == '__main__':
     unittest.main()
