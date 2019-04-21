06f6b281ee7215bc0e53038286fd7ba518548dc1
Adi Suresh <suresh.aditya@gmail.com>
2019-04-21 13:52:28 -0700

    clean up crawler helper methods

--- crawler/helpers.py
+++ crawler/helpers.py
@@ -5,11 +5,9 @@ import flask
 from google.cloud import storage
 import io
 import logging
-import re
+import os
 import redis
 import requests
-from requests.exceptions import HTTPError
-import os
 import uuid
 
 import app
@@ -40,36 +38,21 @@ def store_response_in_gcs(res, key):
     storage_client = storage.Client()
     bucket = storage_client.get_bucket(os.environ['GCS_BUCKET'])
     blob = bucket.blob(key)
-    try:
-        blob.upload_from_string(res.text)
-        blob.make_public()
-    except Exception as e:
-        app.context.logger.error("Could not store response in GCS: %s", str(e))
-    else:
-        return blob.public_url
-    return ""
+    blob.upload_from_string(res.text)
+    blob.make_public()
+    return blob.public_url
 
 
 # get_links - parses the url 'a' tags using beautiful soup
-def get_links(r):
-    bs_obj = bs4.BeautifulSoup(r.content, 'html.parser')
-    links_obj = {}
+def get_links(response):
+    bs_obj = bs4.BeautifulSoup(response.content, 'html.parser')
+    urls = {}
     for link in bs_obj.find_all('a'):
         if 'href' in link.attrs:
-            links_obj[link.attrs['href']] = 1
+            url = link.attrs['href']
+            if url in urls:
+                urls[url] = urls[url] + 1
+            else:
+                urls[url] = 1
 
-    try:
-        return list(links_obj.keys())
-    except Exception as e:
-        app.context.logger.error("Could not list links in url: %s", str(e))
-        return []
-
-# add url and s3 location to central redis cache
-def add_key_cache(url,s3uri):
-    if redis_connect.redisConnect is None:
-       return
-    try:
-        redis_connect.redisConnect.set(url,s3uri)
-    except Exception as e:
-        print(e.with_traceback)
-    return
+    return list(urls.keys())
