1763131394c651cea960eff529510fa86f115278
aniket <aniket02shah@gmail.com>
2019-04-23 22:07:47 -0400

    completed helper tests

--- crawler-manager/helpers_test.py
+++ crawler-manager/helpers_test.py
@@ -30,8 +30,8 @@ class TestCrawlerManagerHelpers(unittest.TestCase):
         self.assertEqual(get_root_url('https://www.test.com/abc/def/123/ghi'),'https://www.test.com/','Correct domain name not returned')
     
     def test_is_absolute_url(self):
-        self.assertEqual(is_absolute_ur('https://www.test.com/abc/'),True,'Absolute url not detected')
-        self.assertEqual(is_absolute_ur('https://www.test.com/'),True,'Absolute url not detected')
+        self.assertEqual(is_absolute_url('https://www.test.com/abc/'),True,'Absolute url not detected')
+        self.assertEqual(is_absolute_url('https://www.test.com/'),True,'Absolute url not detected')
         self.assertEqual(is_absolute_url('https://www.test.com/abc/def/123/ghi'),True,'Absolute url not detected')
         self.assertEqual(is_absolute_url('abc/'),False,'Relative url not detected')
         self.assertEqual(is_absolute_url('www.test.com/'),False,'Relative url not detected')
@@ -45,10 +45,12 @@ class TestCrawlerManagerHelpers(unittest.TestCase):
         self.assertEqual(is_root_url('/abc'),False,'Incorrect root url')
 
     def test_strip_protocol_from_url(self):
-        pass
-    
-    def test_upload_public_file(self):
-        pass
+        self.assertEqual(strip_protocol_from_url('https://www.test.com'),'test.com','https not stripped')
+        self.assertEqual(strip_protocol_from_url('https://www.test.com/abc'),'test.com/abc','https not stripped')
+        self.assertEqual(strip_protocol_from_url('http://www.test.com'),'test.com','http not stripped properly')
+        self.assertEqual(strip_protocol_from_url('ftp://www.test.com'),'test.com','ftp not stripped properly')
+        self.assertEqual(strip_protocol_from_url('www.test.com'),'test.com','www not stripped properly')   
+        self.assertEqual(strip_protocol_from_url('ftps://www.test.com'),'ftps://test.com','incorrect replacement')
     
     
  
