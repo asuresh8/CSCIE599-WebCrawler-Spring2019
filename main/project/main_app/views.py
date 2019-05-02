d62cfdb2c7f3117eb44f6efdec7122f8912d8f1b
Ansuman Prusty <ansumanprusty@gmail.com>
2019-05-01 20:34:40 -0400

    Debug

--- main/project/main_app/views.py
+++ main/project/main_app/views.py
@@ -169,7 +169,7 @@ def complete_crawl(request):
     crawl_request.save()
     data = {"CrawlComplete" : "done"}
     logger.info('Crawl-Complete3 id - %s, manifest - %s', id, crawl_request.crawler_manager_endpoint)
-    #requests.post(os.path.join(crawl_request.crawler_manager_endpoint, 'kill'), json={})
+    requests.post(os.path.join(crawl_request.crawler_manager_endpoint, 'kill'), json={})
     logger.info('Crawl-Complete4 id - %s, manifest - %s', id, manifest)
     user = User.objects.get(username=(CRAWLER_MANAGER_USER_PREFIX + str(id)))
     logger.info('Crawl-Complete5 id - %s, manifest - %s', id, manifest)
